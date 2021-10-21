a3128e759e1216ccedc21d7d9d1127e74a3d56eb
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2021-10-21 14:41:41 -0700

    Installed redux-form

--- emaily/routes/surveyRoutes.js
+++ emaily/routes/surveyRoutes.js
@@ -83,10 +83,6 @@ module.exports = (app) => {
     console.log(req.body);
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-
-
-
-    
     _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
