ef7ffe3521e0f9efae403de5378e117cfd463856
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2021-11-07 01:57:55 -0700

    Removed unnecessary comments

--- application/models/User.js
+++ application/models/User.js
@@ -25,39 +25,6 @@ const customSchema = new Schema({
     avatar: String
 });
 
-// Before saving a model, run this function. Encrypt a password using salt. This is called hashing. 
-// customSchema.pre('save', function(next) {
-//     // get access to the user model
-//     const users = this;
-
-//     bcrypt.genSalt(10, function(err, salt) {
-//         if(err) {
-//             return next(err);
-//         }
-
-//         // hash (encrypt) our password using the salt
-//         bcrypt.hash(users.password, salt, null, function(err, hash) {
-//             if(err) {
-//                 return next(err);
-//             }
-
-//             // overwrite plain text password with encrypted password
-//             users.password = hash;
-//             next();
-//         })
-//     }) 
-// })
-
-// customSchema.methods.comparePassword = function(candidatePassword, callback) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//         if(err) {
-//             return callback(err);
-//         }
-
-//         callback(null, isMatch);
-//     });
-// }
-
 customSchema.plugin(passportLocalMongoose);
 
 mongoose.model('users', customSchema);
