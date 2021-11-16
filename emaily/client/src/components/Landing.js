50ce295c3eba3865178b7db9cc778e78f56c7de3
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2021-11-15 23:57:48 -0800

    Moved node-modules folder to the src folder.
    This allowed the node_modules to be affected by the .gitignore file.

--- emaily/client/src/components/Landing.js
+++ emaily/client/src/components/Landing.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 const Landing = () => {
     return (
         <div style = {{ textAlign: 'center' }}>
@@ -10,8 +8,8 @@ const Landing = () => {
                 <br />
                 <b>Instructions: </b>
                 <br />
-                Please log-in to access our websites features. In order to utilize this website fully, you will have to create a sendgrid account and obtain a Sendgrid API key. <b>Warning</b> this is a different key than the Stripe Publishable Key and Stripe Secret Key. Once on the website, verify a single sender under Sender Authentication under Settings. The from email is the one that has to be specified for each new survey created. Under Mail Settings, enable Event Webhook with an HTTP Post URL of https://limitless-citadel-63953.herokuapp.com/api/surveys/webhooks and then leave everything else blank except Engagement Data. Under Engagement Data make sure <b>Clicked</b> is selected.
-                Visit https://app.sendgrid.com/ for more information.
+                Please log-in to access our websites features. In order to utilize this website fully, you will have to create a sendgrid account and obtain a Sendgrid API key. <b>Warning</b> this is a different key than the Stripe Publishable Key and Stripe Secret Key. Once on the website, verify a single sender under Sender Authentication under Settings. The from email is the one that has to be specified for each new survey created. Under Mail Settings, enable Event Webhook with an HTTP Post URL of https://protected-ocean-66873.herokuapp.com/api/surveys/webhooks and then leave everything else blank except Engagement Data. Under Engagement Data make sure <b>Clicked</b> is selected.
+                Visit https://app.sendgrid.com/ for more information. DO NOT SHARE THESE KEYS WITH ANYONE ELSE.
             </h1>
             
         </div>
