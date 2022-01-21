e959785aceeb1abf96d797d35f3a837265b949bf
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2022-01-21 04:09:43 -0800

    Made landing page more mobile-friendly by adding a break separated parts of the links.

--- emaily/client/src/components/Landing.js
+++ emaily/client/src/components/Landing.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div style = {{ textAlign: 'center', minHeight: '2000px' }}>
-            <h1>
+        <div >
+            <h5>
                 Welcome to our website. 
                 <br />
                 <b>Instructions: </b>
                 <br />
-                Please log-in to access our websites features. In order to utilize this website fully, you will have to create a sendgrid account and obtain a Sendgrid API key. <b>Warning</b> this is a different key than the Stripe Publishable Key and Stripe Secret Key. Once on the website, verify a single sender under Sender Authentication under Settings. The from email is the one that has to be specified for each new survey created. Under Mail Settings, enable Event Webhook with an HTTP Post URL of https://protected-ocean-66873.herokuapp.com/api/surveys/webhooks and then leave everything else blank except Engagement Data. Under Engagement Data make sure <b>Clicked</b> is selected.
+                Please log-in to access our websites features. In order to utilize this website fully, you will have to create a sendgrid account and obtain a Sendgrid API key. <b>Warning</b> this is a different key than the Stripe Publishable Key and Stripe Secret Key. Once on the website, verify a single sender under Sender Authentication under Settings. The from email is the one that has to be specified for each new survey created. Under Mail Settings, enable Event Webhook with an HTTP Post URL of https://protected-ocean-66873.herokuapp.com
+                <br />
+                /api/surveys/webhooks and then leave everything else blank except Engagement Data. Under Engagement Data make sure <b>Clicked</b> is selected.
                 Visit https://app.sendgrid.com/ for more information. DO NOT SHARE THESE KEYS WITH ANYONE ELSE.
-            </h1>
+            </h5>
             
         </div>
     )
